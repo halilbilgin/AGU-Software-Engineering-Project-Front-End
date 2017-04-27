de739160d6b4c68d43a7447973ee1d556d799fef
Elif Tultay <eliftultay@gmail.com>
2017-04-27 09:05:53 +0300

    Update websocket.js

--- js/websocket.js
+++ js/websocket.js
@@ -17,40 +17,80 @@ ws.onmessage = function (evt)
 {
   var received_msg = JSON.parse(evt.data);
   console.log(received_msg);
-    Highcharts.chart('container', {
-    chart: {
-        type: 'line'
-    },
-    title: {
-        text: 'Monthly Average Temperature'
-    },
-    subtitle: {
-        text: 'Source: WorldClimate.com'
-    },
-    xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    },
-    yAxis: {
-        title: {
-            text: 'Device Info'
-        }
-    },
-    plotOptions: {
-        line: {
-            dataLabels: {
-                enabled: true
-            },
-            enableMouseTracking: false
-        }
-    },
-    series: [{
-        name: 'Tokyo',
-        data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }, {
-        name: 'London',
-        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-    }]
-});
+    $(document).ready(function () {
+      Highcharts.setOptions({
+          global: {
+              useUTC: false
+          }
+      });
+
+      Highcharts.chart('container1', {
+          chart: {
+              type: 'spline',
+              animation: Highcharts.svg, // don't animate in old IE
+              marginRight: 10,
+              events: {
+                  load: function () {
+
+                      // set up the updating of the chart each second
+                      var series = this.series[0];
+                      setInterval(function () {
+                          var x = (new Date()).getTime(), // current time
+                            y = Math.random();
+                          series.addPoint([x, y], true, true);
+                      }, 1000);
+                  }
+              }
+          },
+          title: {
+              text: 'Live Data'
+          },
+          xAxis: {
+              type: 'time',
+              tickPixelInterval: 150
+          },
+          yAxis: {
+              title: {
+                  text: 'Value'
+              },
+              plotLines: [{
+                  value: 0,
+                  width: 1,
+                  color: '#808080'
+              }]
+          },
+          tooltip: {
+              formatter: function () {
+                  return '<b>' + this.series.name + '</b><br/>' +
+                      Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x)  + '<br/>' +
+                      Highcharts.numberFormat(this.y, 2);
+              }
+          },
+          legend: {
+              enabled: false
+          },
+          exporting: {
+              enabled: false
+          },
+          series: [{
+              name: 'Random data',
+              data: (function () {
+                  // generate an array of random data
+                  var data = [],
+                      time = (new Date()).getTime(),
+                      i;
+
+                  for (i = -19; i <= 0; i += 1) {
+                      data.push({
+                          x: time + i * 1000,
+                          y: Math.random()
+                      });
+                  }
+                  return data;
+              }())
+          }]
+      });
+  });
 };
 
 ws.onclose = function()
