4971dbbc6a74f9cd101312783307c7c692ae7803
Halil Bilgin <Halil Bilgin>
2017-04-27 11:08:38 +0300

    Websocket update

--- js/websocket.js
+++ js/websocket.js
@@ -6,93 +6,83 @@ if (! ( "WebSocket" in window) ){
 
 // Let us open a web socket
 var ws = new WebSocket("ws://217.112.83.250:443/");
-
+var chart;
+var data = [
+[new Date(0), 0.7695]
+];
 ws.onopen = function()
 {
 // Web Socket is connected, send data using send()
   ws.send("Message to send");
-};
-
-ws.onmessage = function (evt)
-{
-  var received_msg = JSON.parse(evt.data);
-  console.log(received_msg);
-    $(document).ready(function () {
-      Highcharts.setOptions({
+    Highcharts.setOptions({
           global: {
               useUTC: false
           }
       });
 
-      Highcharts.chart('container1', {
-          chart: {
-              type: 'spline',
-              animation: Highcharts.svg, // don't animate in old IE
-              marginRight: 10,
-              events: {
-                  load: function () {
+      chart =Highcharts.chart('container1', {
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Important'
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+        xAxis: {
+        },
+        yAxis: {
+            
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
 
-                      // set up the updating of the chart each second
-                      var series = this.series[0];
-                      setInterval(function () {
-                          var x = (new Date()).getTime(), // current time
-                            y = Math.random();
-                          series.addPoint([x, y], true, true);
-                      }, 1000);
-                  }
-              }
-          },
-          title: {
-              text: 'Live Data'
-          },
-          xAxis: {
-              type: 'time',
-              tickPixelInterval: 150
-          },
-          yAxis: {
-              title: {
-                  text: 'Value'
-              },
-              plotLines: [{
-                  value: 0,
-                  width: 1,
-                  color: '#808080'
-              }]
-          },
-          tooltip: {
-              formatter: function () {
-                  return '<b>' + this.series.name + '</b><br/>' +
-                      Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x)  + '<br/>' +
-                      Highcharts.numberFormat(this.y, 2);
-              }
-          },
-          legend: {
-              enabled: false
-          },
-          exporting: {
-              enabled: false
-          },
-          series: [{
-              name: 'Random data',
-              data: (function () {
-                  // generate an array of random data
-                  var data = [],
-                      time = (new Date()).getTime(),
-                      i;
-
-                  for (i = -19; i <= 0; i += 1) {
-                      data.push({
-                          x: time + i * 1000,
-                          y: Math.random()
-                      });
-                  }
-                  return data;
-              }())
-          }]
-      });
-  });
+        series: [{
+        data: data
+        }]
+    });
+ 
 };
 
+ws.onmessage = function (evt)
+{
+  var received_msg = JSON.parse(evt.data);
+  if(received_msg.generalCharts[0].x == 0)
+    return false;
+ 
+  data.push( [new Date(received_msg.generalCharts[0].x*100), received_msg.generalCharts[0].y]);
+
+  chart.series[0].setData(data, true);
+  }
 ws.onclose = function()
 {
 // websocket is closed.
