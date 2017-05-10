11ff0065c9af62ec921892c2803ae1747d4b176f
Elif Tultay <eliftultay@gmail.com>
2017-05-10 17:22:37 +0300

    Update conf-pane-func.js

--- js/conf-pane-func.js
+++ js/conf-pane-func.js
@@ -1,16 +1,30 @@
-var allDevices = [];
-var allMarkers = [];
-var deviceInfo = [];
-
 $(function() {
-  $(document).on('click', '.add', function() {
+    $(document).on('click', '.add', function() {
 
     deviceInfo = $(this).closest('form').serializeArray();
-    if(allDevices.length==0 && deviceInfo[0].value=='iot-device'){
-      alert('Deploy Base First');
-      deviceInfo=null;
+
+    if(deviceInfo[0].value == 'iot-device'){
+      if (deviceInfo[1].value == 'Hardware Platform' || deviceInfo[2].value == 'OS Image'
+         || deviceInfo[3].value == 'Location' || deviceInfo[4].value == 'Protocol'
+         || deviceInfo[5].value == null || deviceInfo[6].value == null){
+           alert("Please fill all the parts.");
+           deviceInfo=null;
+         }
+    } else if(deviceInfo[0].value == 'base-stations'){
+      if (deviceInfo[1].value == null || deviceInfo[2].value == null
+         || deviceInfo[3].value == null){
+           alert("Please fill all the parts.");
+           deviceInfo=null;
+         }
     }
-    else alert('Choose a location');
+
+    if(allDevices.length==0 && deviceInfo[0].value=='iot-device'){
+    alert('Deploy Base First');
+      deviceInfo=null;
+   }
+     else alert('Choose a location');
+
+
 
   })
 })
