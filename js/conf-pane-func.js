0a1de66324bd9d79cb765c77ad5cafb7f045c891
skubra <skklc.123@gmail.com>
2017-05-10 16:29:02 +0300

    Update conf-pane-func.js

--- js/conf-pane-func.js
+++ js/conf-pane-func.js
@@ -1,13 +1,16 @@
 var allDevices = [];
 var allMarkers = [];
-
 var deviceInfo = [];
 
 $(function() {
   $(document).on('click', '.add', function() {
 
     deviceInfo = $(this).closest('form').serializeArray();
-    alert('Choose a location');
+    if(allDevices.length==0 && deviceInfo[0].value=='iot-device'){
+      alert('Deploy Base First');
+      deviceInfo=null;
+    }
+    else alert('Choose a location');
 
   })
 })
