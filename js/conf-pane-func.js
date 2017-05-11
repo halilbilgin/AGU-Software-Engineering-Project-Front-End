baeebd73cdf4207ed4b0d4f4731c472eff8fbd64
skubra <skklc.123@gmail.com>
2017-05-11 05:49:56 +0300

    Update conf-pane-func.js

--- js/conf-pane-func.js
+++ js/conf-pane-func.js
@@ -2,38 +2,40 @@ $(function() {
   function iotCheck () {
     check = deviceInfo[1].value == 'Hardware Platform' || deviceInfo[2].value == 'OS Image'
        || deviceInfo[3].value == 'Location' || deviceInfo[4].value == 'Protocol'
-       || deviceInfo[5].value == null || deviceInfo[6].value == null;
+       || deviceInfo[5].value == "" || deviceInfo[6].value == "";
 
-    return ! check;
+    return check;
   }
 
   function baseCheck() {
-    check = deviceInfo[1].value == null || deviceInfo[2].value == null
-       || deviceInfo[3].value == null;
+    check = deviceInfo[1].value == "" || deviceInfo[2].value == ""
+       || deviceInfo[3].value == "";
 
-    return baseCheck;
+    return check;
   }
     $(document).on('click', '.add', function() {
 
     deviceInfo = $(this).closest('form').serializeArray();
 
     if(deviceInfo[0].value == 'iot-device'){
-      if (! iotCheck()){
+      if (iotCheck()){
            alert("Please fill all the parts.");
            deviceInfo=null;
          }
     } else if(deviceInfo[0].value == 'base-stations'){
-      if (! baseCheck()){
+      if (baseCheck()){
            alert("Please fill all the parts.");
            deviceInfo=null;
          }
     }
 
-    if(allDevices.length==0 && deviceInfo[0].value=='iot-device'){
-    alert('Deploy Base First');
-      deviceInfo=null;
-   }
-     else alert('Choose a location');
+    if(deviceInfo!=null){
+      if(allDevices.length==0 && deviceInfo[0].value=='iot-device'){
+        alert('Deploy Base First');
+        deviceInfo=null;
+      }
+       else alert('Choose a location');
+    }
 
   })
 
@@ -45,7 +47,7 @@ $(function() {
       deviceInfo=null;
     }
 
-    if(! iotCheck() || deviceInfo[7].value == null) {
+    if(iotCheck() || deviceInfo[7].value == null) {
       alert("Please fill all the parts.");
       return false;
     }
